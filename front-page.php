faaf51c7d68a063046865653f32379bd23c5fd8b
Base5 Designs <dizzy@base5designs.co.uk>
2017-02-20 21:04:46 +0100

    updated grid layout

--- front-page.php
+++ front-page.php
@@ -10,16 +10,16 @@
 get_header(); ?>
 	<div class="row">
 
-		<div class="small-10 small-offset-1 medium-3 medium-offset-1 large-3 large-offset-1">
+		<div class="hide-for-small-only medium-2 medium-offset-1 ">
 			<?php do_action( 'did_displays_menu' ); ?>
-			<br><br>
-			<div class="di-border">
-				<a class="twitter-timeline" data-height="600" data-dnt="true" href="https://twitter.com/Display_Innov">Tweets
-					by Display_Innov</a>
-				<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+			<br>
+			<br>
+			<a class="twitter-timeline" data-height="600" data-dnt="true" href="https://twitter.com/Display_Innov">Tweets
+				by Display_Innov</a>
+			<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
 		</div>
-		</div>
-		<div class="small-10 small-offset-1 medium-6 medium-offset-1 large-6 large-offset-1">
+
+		<div class="small-10 small-offset-1 small-push-1 medium-7 medium-offset-1 medium-push-1">
 			<div class="container">
 				<?php do_action( 'did_display_icons' ); ?>
 			</div>
