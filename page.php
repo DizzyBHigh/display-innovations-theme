0957d7351084e27c9be4d19b0bdeaa5d1cf54007
Base5 Designs <dizzy@base5designs.co.uk>
2017-02-20 21:06:55 +0100

    removed did_displays menu for small screens

--- page.php
+++ page.php
@@ -20,21 +20,20 @@ get_header();
 			<?php do_action( 'did_displays_menu' ); ?>
 		</div>
 		<div class="small-10 small-offset-1 small-push-1 medium-7 medium-offset-1 medium-push-1">
+			<div class="display-holder">
+				<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<div class="row">
-					<div class="small-12 medium-12">
-						<h2><?php the_title(); ?></h2>
-						<?php the_content(); ?>
+					<div class="row">
+						<div class="small-12 medium-12">
+							<h2><?php the_title(); ?></h2>
+							<?php the_content(); ?>
+						</div>
 					</div>
-				</div>
 
-			<?php endwhile; // End of the loop. ?>
+				<?php endwhile; // End of the loop. ?>
+			</div>
 		</div>
-		<div class="show-for-small-only small-offset-1 small-10 small-push-1">
-			<?php do_action( 'did_displays_menu' ); ?>
-		</div>
+
 	</div>
 <?php
 get_footer();
