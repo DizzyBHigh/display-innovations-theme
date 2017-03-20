0957fd97b01a10f82746a7052ec46b2ba42fd379
Base5 Designs <dizzy@base5designs.co.uk>
2017-03-20 19:42:06 +0100

    New changelist

--- assets/dist/js/app.js
+++ assets/dist/js/app.js
@@ -1,5 +1,5 @@
-jQuery(document).ready(function (e) {
-    e(document).foundation(), e(".fancybox").attr("rel", "casestudy").attr("rel", "images").attr("rel", "video").fancybox({
+jQuery(document).ready(function (t) {
+    t(document).foundation(), $options = {
         openEffect: "none",
         closeEffect: "none",
         nextEffect: "none",
@@ -11,29 +11,20 @@ jQuery(document).ready(function (e) {
         height: 520,
         margin: [20, 60, 20, 60],
         beforeLoad: function () {
-            var t, i = e(this.element).data("cs-title");
-            i && (t = e("#cs-title-" + i), t.length && (this.title = t.html() + "<br><i>Image " + (this.index + 1) + " of " + this.group.length + "</i>"));
-            var n, o = e(this.element).data("i-title");
-            o && (n = e("#i-title-" + o), n.length && (this.title = n.html() + "<br><i>Image " + (this.index + 1) + " of " + this.group.length + "</i>"));
-            var a, l = e(this.element).data("v-title");
-            l && (a = e("#v-title-" + l), a.length && (this.title = a.html() + "<br><i>Video " + (this.index + 1) + " of " + this.group.length + "</i>", e(".fancybox-inner").attr("height", 400), e.fancybox.update()))
+            var e, n = t(this.element).data("cs-title");
+            n && (e = t("#cs-title-" + n), e.length && (this.title = e.html() + "<br><i>Image " + (this.index + 1) + " of " + this.group.length + "</i>"));
+            var i, o = t(this.element).data("i-title");
+            o && (i = t("#i-title-" + o), i.length && (this.title = i.html() + "<br><i>Image " + (this.index + 1) + " of " + this.group.length + "</i>"));
+            var a, s = t(this.element).data("v-title");
+            s && (a = t("#v-title-" + s), a.length && (this.title = a.html() + "<br><i>Video " + (this.index + 1) + " of " + this.group.length + "</i>", t(".fancybox-inner").attr("height", 400), t.fancybox.update()))
         },
         afterLoad: function () {
-            if ("inline" == this.type) {
-                var t = e(this.element).data("v-title");
-                this.height = e(".videoItem" + t).height() + 35;
-                var i = e(".videoItem" + t).attr("id"), n = videojs(i);
-                console.log(n), n.loop(!1), n.muted(!1), n.play(!0)
-            }
+            "inline" == this.type
         },
         helpers: {title: {type: "inside"}}
-    }), e.fancybox.update();
-    var t = e(".videoplayer");
-    e.each(t, function () {
-        player = videojs(this.id), console.log(player), player.autoplay(!1), player.loop(!1), player.muted(!1), player.pause(!0)
-    });
-    var i = e(".owl-carousel");
-    i.owlCarousel({
+    }, t(".fancybox").attr("rel", "casestudy").fancybox($options), t(".fancybox").attr("rel", "images").fancybox($options), t(".fancybox").attr("rel", "video").fancybox($options), t.fancybox.update();
+    var e = t(".owl-carousel");
+    e.owlCarousel({
         items: 1,
         center: !0,
         loop: !0,
@@ -41,15 +32,26 @@ jQuery(document).ready(function (e) {
         dots: !1,
         autoplay: !0,
         autoplaySpeed: 1e3
-    }), e(".di-prev-slide").on("click", function () {
-        i.trigger("prev.owl.carousel", [1e3])
-    }), e(".di-next-slide").on("click", function () {
-        i.trigger("next.owl.carousel", [1e3])
-    })
+    }), t(".di-prev-slide").on("click", function () {
+        e.trigger("prev.owl.carousel", [1e3])
+    }), t(".di-next-slide").on("click", function () {
+        e.trigger("next.owl.carousel", [1e3])
+    });
+    var n = n || [];
+    n.push(["_setAccount", "UA-27695900-1"]), n.push(["_trackPageview"]), function () {
+        var t = document.createElement("script");
+        t.type = "text/javascript", t.async = !0, t.src = ("https:" == document.location.protocol ? "https://ssl" : "http://www") + ".google-analytics.com/ga.js";
+        var e = document.getElementsByTagName("script")[0];
+        e.parentNode.insertBefore(t, e)
+    }(), checkBorders = function () {
+        t("#twitter-widget-0").contents().find(".timeline-Widget").css("border", "1px solid #146BAB").css("margin-top", "20px").css("border-radius", "10px"), t("#twitter-widget-0").contents().find(".timeline-Viewport").css("height", "604px")
+    }, setInterval(function () {
+        checkBorders()
+    }, 1e3)
 }), function () {
-    var e = navigator.userAgent.toLowerCase().indexOf("webkit") > -1, t = navigator.userAgent.toLowerCase().indexOf("opera") > -1, i = navigator.userAgent.toLowerCase().indexOf("msie") > -1;
-    (e || t || i) && document.getElementById && window.addEventListener && window.addEventListener("hashchange", function () {
-        var e, t = location.hash.substring(1);
-        /^[A-z0-9_-]+$/.test(t) && (e = document.getElementById(t), e && (/^(?:a|select|input|button|textarea)$/i.test(e.tagName) || (e.tabIndex = -1), e.focus()))
+    var t = navigator.userAgent.toLowerCase().indexOf("webkit") > -1, e = navigator.userAgent.toLowerCase().indexOf("opera") > -1, n = navigator.userAgent.toLowerCase().indexOf("msie") > -1;
+    (t || e || n) && document.getElementById && window.addEventListener && window.addEventListener("hashchange", function () {
+        var t, e = location.hash.substring(1);
+        /^[A-z0-9_-]+$/.test(e) && (t = document.getElementById(e), t && (/^(?:a|select|input|button|textarea)$/i.test(t.tagName) || (t.tabIndex = -1), t.focus()))
     }, !1)
 }();
