4826df1c8bdfbf3ebc3dbc49d0f04d46590cdb45
Base5 Designs <dizzy@base5designs.co.uk>
2017-04-07 11:30:01 +0200

    chabged text area for realted displays to wysiwyg,

--- includes/cpt-displays.php
+++ includes/cpt-displays.php
@@ -386,14 +386,44 @@ function cmb2_display_metaboxes() {
 		'desc'    => 'This text will appear below the Case Studies Button and Above the Top Related displays.',
 		'default' => '',
 		'id'      => $prefix . 'additional_text_one',
-		'type'    => 'textarea'
+		'type'    => 'wysiwyg',
+		'options' => array(
+			'wpautop'       => true,
+			// use wpautop?
+			'media_buttons' => true,
+			// show insert/upload button(s)
+			//'textarea_name' => $editor_id, // set the textarea name to something different, square brackets [] can be used here
+			//'textarea_rows' => get_option('default_post_edit_rows', 10), // rows="..."
+			//'tabindex' => '',
+			//'editor_css' => '', // intended for extra styles for both visual and HTML editors buttons, needs to include the `<style>` tags, can use "scoped".
+			//'editor_class' => '', // add extra class(es) to the editor textarea
+			//'teeny' => false, // output the minimal editor config used in Press This
+			//'dfw' => false, // replace the default fullscreen with DFW (needs specific css)
+			//'tinymce' => true, // load TinyMCE, can be used to pass settings directly to TinyMCE using an array()
+			//'quicktags' => true // load Quicktags, can be used to pass settings directly to Quicktags using an array()
+		),
 	) );
 	$cmbAdditionalText->add_field( array(
 		'name'    => 'Text Below Case studies',
 		'desc'    => 'This text will appear below the Top Related displays and Above Page Images.',
 		'default' => '',
 		'id'      => $prefix . 'additional_text_two',
-		'type'    => 'textarea'
+		'type'    => 'wysiwyg',
+		'options' => array(
+			'wpautop'       => true,
+			// use wpautop?
+			'media_buttons' => true,
+			// show insert/upload button(s)
+			//'textarea_name' => $editor_id, // set the textarea name to something different, square brackets [] can be used here
+			//'textarea_rows' => get_option('default_post_edit_rows', 10), // rows="..."
+			//'tabindex' => '',
+			//'editor_css' => '', // intended for extra styles for both visual and HTML editors buttons, needs to include the `<style>` tags, can use "scoped".
+			//'editor_class' => '', // add extra class(es) to the editor textarea
+			//'teeny' => false, // output the minimal editor config used in Press This
+			//'dfw' => false, // replace the default fullscreen with DFW (needs specific css)
+			//'tinymce' => true, // load TinyMCE, can be used to pass settings directly to TinyMCE using an array()
+			//'quicktags' => true // load Quicktags, can be used to pass settings directly to Quicktags using an array()
+		),
 	) );
 
 }
